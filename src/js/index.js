7ecb0707576cbaad1b8f313e9f1084de7a5c0ae7
Tim Johnson <tjohnson@scitent.com>
2017-01-13 19:25:11 -0500

    clean up some linting issues

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,20 @@
 /* eslint-env browser, jquery */
 import $ from './theme';
-import jQuery from './activity_lifecycle';
+import './activity_lifecycle';
 import './custom_elements';
 import cheet from 'cheet.js';
 import './section_prefetch';
 import './hucklebuck';
 import './progress';
 import './scroll_to_top';
+import './i18n';
+import './404';
 
 
+const konami_pop = () => window.alert('Konàmi!');
 
-const konami_pop = () => alert('Konàmi!');
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', konami_pop);
 
-cheet('↑ ↑ ↓ ↓ ← → ← → b a', konami_pop );
-
-
-//
-// var content = document.getElementById("content");
-// content.innerHTML = '<x-hola ref="somewhere nice"></x-hello>';
-
-
+// cheet('↑ ↑ ↓ ↓ ', run_tranny );
 $(document).trigger('activity::ready');
 $(document).trigger('sections::prefetch');
--- src/js/section_manifest.js
+++ src/js/section_manifest.js
@@ -1,7 +1,8 @@
 const manifest = [
-  {slug: 'intro_timothy_johnson', title: 'Introducing Timothy Johnson'},
-  {slug: 'bio_timo_jojo', title: 'bio for timo jojo'},
-  {slug: 'contact_timo_jojo', title: 'Contact timo jojo'},
+  { slug: 'intro_timothy_johnson', title: 'Introducing Timothy Johnson' },
+  { slug: 'bio_timo_jojo', title: 'bio for timo jojo' },
+  { slug: 'contact_timo_jojo', title: 'Contact timo jojo' },
+  { slug: '404', title: '404' },
 ];
 
 
