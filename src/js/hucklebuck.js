1ccbf7bd12ae40dbb41069ca728112d829bb7cb2
Tim Johnson <tjohnson@scitent.com>
2017-01-13 19:19:23 -0500

    fixed up some hucklebuck functions

--- src/js/hucklebuck.js
+++ src/js/hucklebuck.js
@@ -4,20 +4,24 @@ import manifest from './section_manifest';
 import './section_prefetch';
 import './hucklebuck_buttons';
 
-const { fetch, sections_prefetched } = window;
+const { sections_prefetched } = window;
 
+
+const renderSectionBySlug = (slug) => {
+  console.log(`Rendering Section by Slug: ${slug}`);
+  if (sections_prefetched[slug] !== undefined) {
+    document.getElementById('content').innerHTML = sections_prefetched[slug];
+    $(document).trigger('hucklebucked', [slug]);
+  } else {
+    document.getElementById('content').innerHTML = sections_prefetched['404'];
+    $(document).trigger('404page');
+  }
+};
 
 const renderSectionByNumber = (i) => {
   console.log(`Rendering Section by #${i}`);
   renderSectionBySlug(manifest[i].slug);
 };
-
-const renderSectionBySlug = (slug) => {
-  console.log(`Rendering Section by Slug: ${slug}`);
-  let content_div = document.getElementById("content");
-  content_div.innerHTML = window.sections_prefetched[slug];
-  $(document).trigger('hucklebucked', [slug]);
-}
 
 const hucklebuck = (slug_or_num) => {
   if (Number.isInteger(slug_or_num)) {
@@ -29,12 +33,12 @@ const hucklebuck = (slug_or_num) => {
 };
 
 $(document).on('hucklebuck', (event, slug_or_num) => {
-  hucklebuck(slug_or_num)
+  hucklebuck(slug_or_num);
 });
 
 const loadFirstPage = () => {
   console.log('loading First Page');
   $(document).trigger('hucklebuck', [0]);
-}
+};
 
 $(document).on(`prefetch::done::${manifest[0].slug}`, () => loadFirstPage());
